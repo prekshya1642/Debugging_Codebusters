dd6bea71a16143825f4d920bc0cb9c0e5d92442a
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-10-13 21:30:45 +1100

    Bug 4
    Bug 4

--- CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Main.java
+++ CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		
 	   BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-
+	 int player_age;
         Dice d1 = new Dice();
         Dice d2 = new Dice();
         Dice d3 = new Dice();
@@ -17,9 +17,20 @@ public class Main {
 
         int totalWins = 0;
         int totalLosses = 0;
- // Code should be added here asking the player to input his age
+ 
         while (true)
         {
+	// Code should be added here asking the player to input his age
+		System.out.print("Enter your age: ");
+        	try{
+        		player_age = Integer.parseInt(console.readLine());
+        		if(player_age > 0 && player_age < 100)
+        			break; // Valid age entered
+        	}catch(Exception e){
+        		// Invalid age
+        		System.out.println("Invalid age! Please try again!");
+        	}
+        	}
             int winCount = 0;
             int loseCount = 0;
             
