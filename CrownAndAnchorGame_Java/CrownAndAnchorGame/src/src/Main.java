8f8e9a182db6d6d55a9f0ad9f0e045263904f0de
prekshya1642 <30651774+prekshya1642@users.noreply.github.com>
2017-10-13 21:48:38 +1100

    Bug 1
    fixing

--- CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Main.java
+++ CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Main.java
@@ -37,6 +37,8 @@ public class Main {
         	System.out.println("Player under 18 is not allowed to play the game. Terminate now!");
         	return;
         }
+	 while (true)
+        {
             int winCount = 0;
             int loseCount = 0;
             
@@ -61,9 +63,21 @@ public class Main {
                    
                 	System.out.printf("Turn %d: %s bet %d on %s\n",
                 			turn, player.getName(), bet, pick); 
-                	
+			// Testing incorrect balance increase on winning
+                	//System.out.println("----- Main.java: Starting calculation of winnings..." );
+                	//System.out.println("----- Main.java: Balance before play: " + player.getBalance());
+                
+                	// Bug 3: DiceValues remain the same all the time
+                	//System.out.println("--- BUG 3 detector: old dice values: " + cdv.get(0) + "," + cdv.get(1) + "," + cdv.get(2));
                 	int winnings = game.playRound(player, pick, bet);
+                	//System.out.println("----- Main.java: Winning amount: " + winnings);
+                	//System.out.println("----- Main.java: Balance after play: " + player.getBalance());
+                	
                     cdv = game.getDiceValues();
+                    //System.out.println("--- BUG 3 detector: new dice values: " + cdv.get(0) + "," + cdv.get(1) + "," + cdv.get(2));
+                    
+                	
+                	
                     
                     System.out.printf("Rolled %s, %s, %s\n",
                     		cdv.get(0), cdv.get(1), cdv.get(2));
@@ -78,7 +92,12 @@ public class Main {
 	                    		player.getName(), player.getBalance());
 	                	loseCount++;
                     }
-                    
+                    //code added to test the loop of it continues when balance still positive
+                    //System.out.println("---- Debugging: Now balance: " + player.getBalance() + ". Bet: " + bet);
+                    //System.out.println("---- Does balance exceeds limit by " + bet + "?: " + player.balanceExceedsLimitBy(bet));
+                    //System.out.println("---- Is balance less than 200?: " + (player.getBalance() < 200));
+                    //System.out.println("---- Will the loop continue?: " + (player.balanceExceedsLimitBy(bet) && player.getBalance()<200));
+                  
                 } //while
 
                 System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
@@ -92,6 +111,7 @@ public class Main {
 
             String ans = console.readLine();
             if (ans.equals("q")) break;
+
         } //while true
         
         System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
